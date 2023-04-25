75f90520179e3244b3a08ee894b971c4ebc74d83
Suwijak Srikhao <tes.20545@gmail.com>
2023-04-25 22:04:11 +0700

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -58,6 +58,34 @@
                                             {{ __('แดชบอร์ด') }}
                                         </x-nav-link>
                                     </li>
+                                    @if (auth()->user()->position == 'user')
+                                        <li>
+                                            <x-nav-link :href="route('helpdesk.index')" :active="request()->routeIs('helpdesk.*')">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                    class="h-6 w-6 shrink-0">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
+                                                </svg>
+                                                {{ __('แจ้งซ่อม') }}
+                                            </x-nav-link>
+                                        </li>
+                                    @endif
+
+                                    @if (auth()->user()->position == 'admin')
+                                        <li>
+                                            <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.*')">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                    class="h-6 w-6 shrink-0">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                                                </svg>
+
+                                                {{ __('ผู้ใช้งาน') }}
+                                            </x-nav-link>
+                                        </li>
+                                    @endif
                                     <li>
                                         <a href="#"
                                             class="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
@@ -168,27 +196,31 @@
                                     {{ __('แดชบอร์ด') }}
                                 </x-nav-link>
                             </li>
-                            @if(auth()->user()->position == 'user')   
-                            <li>
-                                <x-nav-link :href="route('helpdesk.index')" :active="request()->routeIs('helpdesk.*')">                              
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 shrink-0">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
-                                    </svg>                                      
-                                    {{ __('แจ้งซ่อม') }}
-                                </x-nav-link>
-                            </li>          
+                            @if (auth()->user()->position == 'user')
+                                <li>
+                                    <x-nav-link :href="route('helpdesk.index')" :active="request()->routeIs('helpdesk.*')">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor" class="h-6 w-6 shrink-0">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
+                                        </svg>
+                                        {{ __('แจ้งซ่อม') }}
+                                    </x-nav-link>
+                                </li>
                             @endif
 
-                            @if(auth()->user()->position == 'admin')   
-                            <li>
-                                <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.*')">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 shrink-0">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
-                                    </svg>                                      
-                                    
-                                    {{ __('ผู้ใช้งาน') }}
-                                </x-nav-link>
-                            </li>          
+                            @if (auth()->user()->position == 'admin')
+                                <li>
+                                    <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.*')">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor" class="h-6 w-6 shrink-0">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                                        </svg>
+
+                                        {{ __('ผู้ใช้งาน') }}
+                                    </x-nav-link>
+                                </li>
                             @endif
                             <li>
                                 <a href="#"
